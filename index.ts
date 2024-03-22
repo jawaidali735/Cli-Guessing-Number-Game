2320bb1ecd4424b15ff5719fb0d0585274b7075a
Jawaid Ali <jawaidaliupdates@gmail.com>
2024-03-23 02:32:06 +0500

    push my guess number game to github

---
+++ index.ts
@@ -0,0 +1,20 @@
+
+import inquirer from "inquirer";
+
+let randomNumber = Math.floor(Math.random() * 2 + 1);
+console.log("Welcome to the Game");
+
+let answer = await inquirer.prompt([{
+    name: "guess",
+    type: "number",
+    message: "Guess the number"
+
+}])
+
+if(answer.guess === randomNumber){
+    console.log("Congratualations! You gueesed right number.")
+}
+else{
+    console.log("Wrong, you guessed wrong number.")
+}
+
